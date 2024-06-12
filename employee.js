36062f91b64d29a0cc24c1e1a5c78a68faa96bfe
SwayamDhanawade <swayamd1203@gmail.com>
2024-06-12 11:03:14 +0530

    Employee attendance check

--- employee.js
+++ employee.js
@@ -1,2 +1,13 @@
 //-----------UC1-------------
 console.log("Welcome to Employee Wage Computation.")
+
+function employeeCheck() {
+    const empCheck = Math.floor(Math.random() * 2)
+
+    if(empCheck == 1){
+        console.log("Employee is Present.")
+    } else {
+        console.log("Employee is Absent.")
+    }    
+}
+employeeCheck();
