2530b2f8e60dbe562b84974c9cc1d67f380cfe00
SwayamDhanawade <swayamd1203@gmail.com>
2024-06-12 11:08:41 +0530

    monthly wage of employee

--- employee.js
+++ employee.js
@@ -38,3 +38,28 @@ function wage() {
     console.log("Daily Wage of an employee: " + dailyWage)
 }
 wage();
+
+//-----------UC3-------------
+function wageForMonth() {
+    const workingDays = 20
+    let totalWage = 0
+
+    for(let day = 0; day < workingDays; day++){
+        const empCheck = Math.floor(Math.random() * 3)
+        let dailyWage = 0
+    switch (empCheck) {
+        case 0:
+            dailyWage = 0
+            break;
+        case 1:
+            dailyWage = perHourWage * partTime
+            break;
+        case 2:
+            dailyWage = perHourWage * fullTime
+            break;      
+    }
+    totalWage += dailyWage;
+    }
+    return totalWage;
+}
+console.log("Wages for a month: " + wageForMonth());
