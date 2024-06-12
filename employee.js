35c0f1afc71c4a4fcc00e9a1f417e7ce27144ef2
SwayamDhanawade <swayamd1203@gmail.com>
2024-06-12 11:06:28 +0530

    daily wage of employee

--- employee.js
+++ employee.js
@@ -11,3 +11,30 @@ function employeeCheck() {
     }    
 }
 employeeCheck();
+
+//-----------UC2-------------
+const partTime = 4
+const fullTime = 8
+const perHourWage = 20
+
+function wage() {
+    const empCheck = Math.floor(Math.random() * 3)
+
+    let dailyWage = 0
+    switch (empCheck) {
+        case 0:
+            console.log("Employee is Absent.")
+            dailyWage = 0
+            break;
+        case 1:
+            console.log("Employee is working half day.") 
+            dailyWage = perHourWage * partTime
+            break;
+        case 2:
+            console.log("Employee is working full day.") 
+            dailyWage = perHourWage * fullTime
+            break;      
+    }
+    console.log("Daily Wage of an employee: " + dailyWage)
+}
+wage();
